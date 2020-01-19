bafcbcc3ccf9aa2e228da44569c09b577518e668
ChamudiniAthukorala <chamudiniathu@gmail.com>
2020-01-19 16:29:33 +0530

    Hirusha update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import HeaderNotice from './Components/HeaderNotice'; 
-import Header from './Components/Header';
-import './index.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
-import SlideView from './Components/SlideView';
+//Hirusha 1 step add routers libraries.
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import HeaderNotice from "./Components/HeaderNotice";
+import Header from "./Components/Header";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <HeaderNotice></HeaderNotice>
+//        <Header></Header>
+//        <SlideView></SlideView>
+//        <br/>
+//        <Content></Content>
+//        <Footer></Footer>
+//     </div>
+
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-       <HeaderNotice></HeaderNotice>
-       <Header></Header>
-       <SlideView></SlideView>
+    <Router>
+      <div className="App">
+        <HeaderNotice></HeaderNotice>
+        <Header></Header>
+        {/* <SlideView></SlideView>
        <br/>
-       <Content></Content>
-       <Footer></Footer>
-    </div>
+       <Content></Content> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
 
+          {/* Hirusha step 2 - creating routes with assigning router paths */}
 
+        {/* <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} /> */}
+        </Switch>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Content from "./Content";
+import SlideView from "./SlideView";
+
+function Home() {
+  return (
+    <div>
+      <SlideView></SlideView>
+      <br />
+      <Content></Content>
+    </div>
+  );
+}
+export default Home;
